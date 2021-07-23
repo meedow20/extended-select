c5c1b0e0201dab1d3099fe9b9d9242e993065794
meedow20 <55607223+meedow20@users.noreply.github.com>
2021-07-23 14:46:27 +0300

    feat: rewrote everything in the class
    rewrote everything in the class

--- README.md
+++ README.md
@@ -13,3 +13,5 @@
 
 - There is a container on the start page,  **`by clicking`** on which you will be taken to the **`option's selection form`**
 - On the option's selection form, you can **`select options and apply them`** by clicking the **`ПРИМЕНИТЬ`** button, after that the selected options will be displayed **`in the container of the start page`**
+- In the modal window for selecting options, you can search for an option **`by entering values in the input field`**
+- By clicking on the **`Очистить`** button, you can **`reset all selected and founded options`**
--- scripts/extendedSelect.js
+++ scripts/extendedSelect.js
@@ -25,50 +25,57 @@ const AllEnums = {
     tendersAsACustomer: "Тендеры в роли Заказчика",
 }
 
-// App State
-const state = {
-    options: '',
-    selectedOptions: [],
-    countOfSelectedOptions: 0,
-}
+class ExtendedSelect {
 
-// Create Main Form
-let searchSelectContainer = document.createElement('div');
-searchSelectContainer.className = classNameConstants.searchSelectContainer;
-searchSelectContainer.innerHTML = tagConstants.defaultSearchSelectSpan;
-document.body.prepend(searchSelectContainer);
-
-let searchSelectContainerTitle = document.createElement('h2')
-searchSelectContainerTitle.className = classNameConstants.searchSelectContainerTitle;
-searchSelectContainerTitle.innerHTML = AllEnums.tendersAsACustomer;
-document.body.insertBefore(searchSelectContainerTitle, searchSelectContainer);
-
-// Show Modal Form
-searchSelectContainer.addEventListener('click', () => {
-        if (searchSelectModal.className === classNameConstants.visuallyHidden) {
-            [searchSelectContainer.className, searchSelectContainerTitle.className] =
-                [classNameConstants.visuallyHidden, classNameConstants.visuallyHidden];
-            searchSelectModal.className = classNameConstants.searchSelectModal;
-        }
+    constructor(options, selectedOptions, countOfSelectedOptions) {
+        this.options = options;
+        this.selectedOptions = selectedOptions;
+        this.countOfSelectedOptions = countOfSelectedOptions;
     }
-)
 
-// Create Modal Form
-let searchSelectModal = document.createElement('div');
-searchSelectModal.className = classNameConstants.visuallyHidden;
-searchSelectModal.innerHTML = `<div class="searchSelectModalHeader">
+    // Fields
+    searchSelectContainer = document.createElement('div');
+    searchSelectContainerTitle = document.createElement('h2');
+    searchSelectModal = document.createElement('div');
+
+    createSearchSelectContainer() {
+        this.searchSelectContainer.className = classNameConstants.searchSelectContainer;
+        this.searchSelectContainer.innerHTML = tagConstants.defaultSearchSelectSpan;
+        document.body.prepend(this.searchSelectContainer);
+    }
+
+    createSearchSelectContainerTitle() {
+        this.searchSelectContainerTitle.className = classNameConstants.searchSelectContainerTitle;
+        this.searchSelectContainerTitle.innerHTML = AllEnums.tendersAsACustomer;
+        document.body.insertBefore(this.searchSelectContainerTitle, this.searchSelectContainer);
+    }
+
+    addEventToShowModalForm() {
+        this.searchSelectContainer.addEventListener('click', () => {
+                if (this.searchSelectModal.className === classNameConstants.visuallyHidden) {
+                    [this.searchSelectContainer.className, this.searchSelectContainerTitle.className] =
+                        [classNameConstants.visuallyHidden, classNameConstants.visuallyHidden];
+                    this.searchSelectModal.className = classNameConstants.searchSelectModal;
+                }
+            }
+        )
+    }
+
+    createModalForm() {
+        this.searchSelectModal.className = classNameConstants.visuallyHidden;
+        this.searchSelectModal.innerHTML = `<div class="searchSelectModalHeader">
                                    <div class="searchSelectModalTop">
-                                       <button class="searchSelectModalTitle" onclick="displayMainForm()">
+                                       <button class="searchSelectModalTitle">
                                             ${String.fromCodePoint(0x1F814)} Реализуемые товары
                                        </button>
                                        <button class="searchSelectModalStats">
                                            Выбранное (<span class="countOfSelectedOptions" id="countOfSelectedOptions">
-                                                          ${state.countOfSelectedOptions}
+                                                          ${this.countOfSelectedOptions}
                                                       </span>)
                                        </button>
                                    </div>
                                    <div class="searchSelectModalSearch">
-                                       <input type="text" class="searchSelectInput" oninput="searchSelect()" 
+                                       <input type="text" class="searchSelectInput" 
                                        placeholder="Введите код ОКРБ или наименование закупаемой продукции"
                                        >
                                    </div>
@@ -77,122 +84,160 @@ searchSelectModal.innerHTML = `<div class="searchSelectModalHeader">
                         
                                </div>
                                <div class="searchSelectModalFooter">
-                                   <button type="button" class="selectApply" onclick="applySelectedOptions()">Применить</button>
-                                   <button type="button" class="selectReset" onclick="resetSelect()">Очистить</button>
+                                   <button type="button" class="selectApply">Применить</button>
+                                   <button type="button" class="selectReset">Очистить</button>
                                </div>`;
-document.body.prepend(searchSelectModal);
-
-// Selectors
-let countOfSelectedOptions = document.getElementById('countOfSelectedOptions');
-let searchSelectModalMain = document.querySelector('.searchSelectModalMain');
-let searchSelectInput = document.querySelector('.searchSelectInput');
-
-// Modification options from HTML and pushing into Modal Form
-document.querySelectorAll('option').forEach((item, index) => {
-    if (item.selected) {
-        state.options += `<div class="optionContainer" id="option_${index}" 
-                               style="${styleConstants.activeOptionBackgroundColor}">
-                              <input type="checkbox" id="check_${index}" class="optionCheckBox" checked>
-                              <label for="check_${index}" class="optionCheckBoxLabel">${item.outerHTML}</label>
-                          </div>` + '\n'
-        state.countOfSelectedOptions += 1;
-        countOfSelectedOptions.innerText = state.countOfSelectedOptions;
-        state.selectedOptions.push(item.innerText);
-        searchSelectContainer.innerHTML = `<span id='searchSelectSpan'>${state.selectedOptions.join(', ')}</span>`;
-        searchSelectContainer.style.borderLeftColor = styleConstants.searchSelectContainerBorderLeftColor;
-        searchSelectContainer.style.borderLeftWidth = styleConstants.searchSelectContainerBorderLeftWidth;
-    } else {
-        state.options += `<div class="optionContainer" id="option_${index}">
-                              <input type="checkbox" id="check_${index}" class="optionCheckBox">
-                              <label for="check_${index}" class="optionCheckBoxLabel">${item.outerHTML}</label>
-                          </div>` + '\n'
+        document.body.prepend(this.searchSelectModal);
     }
-})
-searchSelectModalMain.innerHTML = state.options;
 
-// Change counter and style when option is selected or unselected
-document.querySelectorAll('.optionCheckBox').forEach((item, index) => item.addEventListener('click',
-    () => {
-        let optionContainer = document.getElementById('option_'.concat(`${index}`));
-        let optionCheckBox = document.getElementById(`${item.id}`);
+    pushingOptions() {
+        let countOfSelectedOptionsSelect = document.getElementById('countOfSelectedOptions');
+        let searchSelectModalMain = document.querySelector('.searchSelectModalMain');
 
-        optionCheckBox.checked ?
-            (
-                state.countOfSelectedOptions += 1,
-                    countOfSelectedOptions.innerText = state.countOfSelectedOptions,
-                    optionContainer.style = styleConstants.activeOptionBackgroundColor,
-                    state.selectedOptions.push(optionContainer.innerText)
-            ) :
-            (
-                state.countOfSelectedOptions -= 1,
-                    countOfSelectedOptions.innerText = state.countOfSelectedOptions,
-                    optionContainer.style.background = styleConstants.none,
-                    state.selectedOptions = state.selectedOptions.filter(item => !optionContainer.innerText.includes(item))
-            )
-    }));
-
-function displayMainForm() {
-    if (searchSelectContainer.className && searchSelectContainerTitle.className === classNameConstants.visuallyHidden) {
-        searchSelectModal.className = classNameConstants.visuallyHidden;
-        [searchSelectContainer.className, searchSelectContainerTitle.className] =
-            [classNameConstants.searchSelectContainer, classNameConstants.searchSelectContainerTitle];
-    }
-}
-
-function applySelectedOptions() {
-    state.selectedOptions.length !== 0 ?
-        (
-            searchSelectContainer.innerHTML = `<span id='searchSelectSpan'>${state.selectedOptions.join(', ')}</span>`,
-                searchSelectContainer.style.borderLeftColor = styleConstants.searchSelectContainerBorderLeftColor,
-                searchSelectContainer.style.borderLeftWidth = styleConstants.searchSelectContainerBorderLeftWidth
-        ) :
-        (
-            searchSelectContainer.innerHTML = tagConstants.defaultSearchSelectSpan,
-                searchSelectContainer.style.borderLeftColor = styleConstants.default,
-                searchSelectContainer.style.borderLeftWidth = styleConstants.default
-        )
-    displayMainForm()
-}
-
-function resetSelect() {
-    document.querySelectorAll('.optionCheckBox').forEach(item => item.checked = false);
-    searchSelectInput.value = '';
-    document.querySelectorAll('.optionContainer').forEach(item => {
-        item.style.background = styleConstants.none
-    });
-    document.querySelectorAll('.hideOptionIfNotSearched').forEach(item => {
-        item.className = classNameConstants.optionContainer;
-        item.style.background = styleConstants.none
-    });
-    [state.countOfSelectedOptions, state.selectedOptions, countOfSelectedOptions.innerText] = [0, [], 0]
-}
-
-function searchSelect() {
-    let searchValue = searchSelectInput.value.trim();
-    let options = document.querySelectorAll('option');
-
-    if (searchValue !== '') {
-        options.forEach((item, index) => {
-            let optionContainer = document.getElementById('option_'.concat(`${index}`));
-            if (item.innerText.toLowerCase().search(searchValue.toLowerCase()) === -1) {
-                if (optionContainer === null) {
-                    return;
-                }
-                optionContainer.className = classNameConstants.hideOptionIfNotSearched;
+        document.querySelectorAll('option').forEach((item, index) => {
+            if (item.selected) {
+                this.options += `<div class="optionContainer" id="option_${index}" 
+                                 style="${styleConstants.activeOptionBackgroundColor}">
+                                     <input type="checkbox" id="check_${index}" class="optionCheckBox" checked>
+                                     <label for="check_${index}" class="optionCheckBoxLabel">${item.outerHTML}</label>
+                                 </div>` + '\n'
+                this.countOfSelectedOptions += 1;
+                countOfSelectedOptionsSelect.innerText = this.countOfSelectedOptions;
+                this.selectedOptions.push(item.innerText);
+                this.searchSelectContainer.innerHTML = `<span id='searchSelectSpan'>${this.selectedOptions.join(', ')}</span>`;
+                this.searchSelectContainer.style.borderLeftColor = styleConstants.searchSelectContainerBorderLeftColor;
+                this.searchSelectContainer.style.borderLeftWidth = styleConstants.searchSelectContainerBorderLeftWidth;
             } else {
-                if (optionContainer === null) {
-                    return;
-                }
-                optionContainer.className = classNameConstants.optionContainer;
+                this.options += `<div class="optionContainer" id="option_${index}">
+                                     <input type="checkbox" id="check_${index}" class="optionCheckBox">
+                                     <label for="check_${index}" class="optionCheckBoxLabel">${item.outerHTML}</label>
+                                 </div>` + '\n'
             }
         })
-    } else {
-        options.forEach((item, index) => {
-            let optionContainer = document.getElementById('option_'.concat(`${index}`));
-            if (optionContainer === null) {
-                return;
+        searchSelectModalMain.innerHTML = this.options;
+    }
+
+    addEventChangeCounterAndStyleWhenOptionSelected() {
+        let countOfSelectedOptionsSelect = document.getElementById('countOfSelectedOptions')
+
+        document.querySelectorAll('.optionCheckBox').forEach((item, index) => item.addEventListener('click',
+            () => {
+                let optionContainer = document.getElementById('option_'.concat(`${index}`));
+                let optionCheckBox = document.getElementById(`${item.id}`);
+
+                optionCheckBox.checked ?
+                    (
+                        this.countOfSelectedOptions += 1,
+                            countOfSelectedOptionsSelect.innerText = this.countOfSelectedOptions,
+                            optionContainer.style = styleConstants.activeOptionBackgroundColor,
+                            this.selectedOptions.push(optionContainer.innerText)
+                    ) :
+                    (
+                        this.countOfSelectedOptions -= 1,
+                            countOfSelectedOptionsSelect.innerText = this.countOfSelectedOptions,
+                            optionContainer.style.background = styleConstants.none,
+                            this.selectedOptions = this.selectedOptions.filter(item => !optionContainer.innerText.includes(item))
+                    )
+            }));
+    }
+
+    displayMainForm() {
+        if (this.searchSelectContainer.className && this.searchSelectContainerTitle.className === classNameConstants.visuallyHidden) {
+            this.searchSelectModal.className = classNameConstants.visuallyHidden;
+            [this.searchSelectContainer.className, this.searchSelectContainerTitle.className] =
+                [classNameConstants.searchSelectContainer, classNameConstants.searchSelectContainerTitle];
+        }
+    }
+
+    addEventClickOnSearchSelectModalTitle() {
+        let searchSelectModalTitle = document.querySelector('.searchSelectModalTitle');
+
+        searchSelectModalTitle.addEventListener('click', () => {
+            this.displayMainForm();
+        })
+    }
+
+    addEventClickOnSelectApply() {
+        let selectApply = document.querySelector('.selectApply');
+
+        selectApply.addEventListener('click', () => {
+            this.selectedOptions.length !== 0 ?
+                (
+                    this.searchSelectContainer.innerHTML = `<span id='searchSelectSpan'>${this.selectedOptions.join(', ')}</span>`,
+                        this.searchSelectContainer.style.borderLeftColor = styleConstants.searchSelectContainerBorderLeftColor,
+                        this.searchSelectContainer.style.borderLeftWidth = styleConstants.searchSelectContainerBorderLeftWidth
+                ) :
+                (
+                    this.searchSelectContainer.innerHTML = tagConstants.defaultSearchSelectSpan,
+                        this.searchSelectContainer.style.borderLeftColor = styleConstants.default,
+                        this.searchSelectContainer.style.borderLeftWidth = styleConstants.default
+                )
+            this.displayMainForm()
+        })
+    }
+
+    addEventClickOnResetSelect() {
+        let selectReset = document.querySelector('.selectReset')
+        let searchSelectInput = document.querySelector('.searchSelectInput');
+        let countOfSelectedOptions = document.getElementById('countOfSelectedOptions');
+
+        selectReset.addEventListener('click', () => {
+            document.querySelectorAll('.optionCheckBox').forEach(item => item.checked = false);
+            searchSelectInput.value = '';
+            document.querySelectorAll('.optionContainer').forEach(item => {
+                item.style.background = styleConstants.none
+            });
+            document.querySelectorAll('.hideOptionIfNotSearched').forEach(item => {
+                item.className = classNameConstants.optionContainer;
+                item.style.background = styleConstants.none
+            });
+            [this.countOfSelectedOptions, this.selectedOptions, countOfSelectedOptions.innerText] = [0, [], 0]
+        })
+    }
+
+    addEventInputOnSearchSelect() {
+        let searchSelectInput = document.querySelector('.searchSelectInput');
+
+        searchSelectInput.addEventListener('input', () => {
+            let searchValue = searchSelectInput.value.trim();
+            let options = document.querySelectorAll('option');
+
+            if (searchValue !== '') {
+                options.forEach((item, index) => {
+                    let optionContainer = document.getElementById('option_'.concat(`${index}`));
+                    if (item.innerText.toLowerCase().search(searchValue.toLowerCase()) === -1) {
+                        if (optionContainer === null) {
+                            return;
+                        }
+                        optionContainer.className = classNameConstants.hideOptionIfNotSearched;
+                    } else {
+                        if (optionContainer === null) {
+                            return;
+                        }
+                        optionContainer.className = classNameConstants.optionContainer;
+                    }
+                })
+            } else {
+                options.forEach((item, index) => {
+                    let optionContainer = document.getElementById('option_'.concat(`${index}`));
+                    if (optionContainer === null) {
+                        return;
+                    }
+                    optionContainer.className = classNameConstants.optionContainer;
+                })
             }
-            optionContainer.className = classNameConstants.optionContainer;
         })
     }
 }
+
+const extendedSelect = new ExtendedSelect('', [], 0);
+extendedSelect.createSearchSelectContainer();
+extendedSelect.createSearchSelectContainerTitle();
+extendedSelect.createModalForm();
+extendedSelect.addEventToShowModalForm();
+extendedSelect.pushingOptions();
+extendedSelect.addEventChangeCounterAndStyleWhenOptionSelected();
+extendedSelect.addEventClickOnSearchSelectModalTitle();
+extendedSelect.addEventClickOnSelectApply();
+extendedSelect.addEventClickOnResetSelect();
+extendedSelect.addEventInputOnSearchSelect();
